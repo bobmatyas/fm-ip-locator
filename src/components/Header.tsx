27af16dd9ef5f07ddb4e2fc7a8f4bd4f6449c093
bob matyas <45246438+bobmatyas@users.noreply.github.com>
2020-11-30 11:41:49 -0500

    add initial components + styled components

---
+++ src/components/Header.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Heading from './Heading'
+
+function Header() {
+  return (
+    <Heading />
+  );
+}
+
+export default Header;
---
+++ src/components/Heading.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Heading() {
+  return (
+    <h1>IP Address Tracker</h1>
+  );
+}
+
+export default Heading;
