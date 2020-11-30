2050f732bfa2d89d7cc1b3753940269181e78d55
bob matyas <45246438+bobmatyas@users.noreply.github.com>
2020-11-30 16:04:05 -0500

    add leaflet

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,20 @@
 import React from 'react';
 import Heading from './Heading'
+import Background from '../images/pattern-bg.png';
+import styled from 'styled-components';
+
+const SiteHeader = styled.header`
+    background-image: url(${Background});
+    background-size: cover;
+    margin: 0;
+    padding: 0;
+`;
 
 function Header() {
   return (
-    <Heading />
+    <SiteHeader>
+        <Heading />
+    </SiteHeader>
   );
 }
 
--- src/components/Heading.tsx
+++ src/components/Heading.tsx
@@ -1,8 +1,19 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const SiteHeader = styled.h1`
+  color: #fff;
+  font-family: 'Rubik', sans-serif;
+  font-size: 2.5rem;
+  font-weight: 500;
+  margin: 0;
+  padding: 35px 0;
+  text-align: center;
+`;
 
 function Heading() {
   return (
-    <h1>IP Address Tracker</h1>
+    <SiteHeader>IP Address Tracker</SiteHeader>
   );
 }
 
