9338f8c4fad4c4eb5b017b44a61d4015abf93074
bob matyas <45246438+bobmatyas@users.noreply.github.com>
2020-12-14 15:56:12 -0500

    add custom icon

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,22 @@ import React from 'react';
 import './App.css';
 import Header from './components/Header';
 import IPBox from './components/IPBox';
-import { MapContainer, TileLayer, Popup, Marker } from 'react-leaflet';
+import { MapContainer, TileLayer, Popup, Marker} from 'react-leaflet';
 import { LatLngTuple } from 'leaflet';
+import L from 'leaflet';
 import styled from 'styled-components';
+import IconLocation from './images/icon-location.svg';
 
 const Wrapper = styled.div`
   margin: 0;
   padding: 0;
   height: 100vh;
 `;
+
+const pointerIcon = new L.Icon({
+  iconUrl: IconLocation,
+  iconRetinaUrl: IconLocation,
+});
 
 function App() {
 
@@ -23,15 +30,13 @@ function App() {
     <Header />
     
     <IPBox />
-    <MapContainer center={position} zoom={zoom} scrollWheelZoom={false}>
+
+    <MapContainer center={position} zoom={zoom} scrollWheelZoom={false} zoomControl={false}>
     <TileLayer
       attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
       url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
     />
-    <Marker position={position}>
-      <Popup>
-        A pretty CSS3 popup. <br /> Easily customizable.
-      </Popup>
+    <Marker position={position} icon={pointerIcon}>
     </Marker>
   </MapContainer>
   </Wrapper>
--- src/components/IPBox.tsx
+++ src/components/IPBox.tsx
@@ -6,9 +6,12 @@ import IPBoxHeading from './IPBox-Heading';
 const IPHolder = styled.div`
     background-color: #fff;
     border-radius: 10px;
-    margin: 0 auto;
+    margin: -50px auto 0 auto;
     padding: 20px 25px;
+    position: absolute;
     width: 325px;
+    z-index: 402;
+
 `;
 
 
@@ -16,7 +19,9 @@ function IPBox() {
   return (
     <IPHolder>
       <IPBoxHeading heading={"IP Address"} value={"127.0.0.1"} />
-
+      <IPBoxHeading heading={"Location"} value={"Brooklyn, NY 10001"} />
+      <IPBoxHeading heading={"Timezone"} value={"UTC-05:00"} />
+      <IPBoxHeading heading={"ISP"} value={"SpaceX StarLink"} />
     </IPHolder>
   );
 }
